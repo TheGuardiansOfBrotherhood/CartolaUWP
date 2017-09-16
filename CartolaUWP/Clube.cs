5367d0a79df320d48a45824727e5871473ba3255
Roger Silva <rogersilva84@gmail.com>
2017-09-16 16:02:16 -0300

    Entidades

---
+++ CartolaUWP/Clube.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartolaUWP
+{
+    [DataContract]
+    class Clube
+    {
+        public Clube()
+        {
+        }
+
+        public Clube(long Id, string Nome, string Abreviacao, Escudos Escudos)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Abreviacao = Abreviacao;
+            this.Escudos = Escudos;
+        }
+
+        [DataMember(Name = "id")]
+        public long Id { get; set; }
+
+        [DataMember(Name = "nome")]
+        public string Nome { get; set; }
+
+        [DataMember(Name = "abreviacao")]
+        public string Abreviacao { get; set; }
+
+        [DataMember(Name = "escudos")]
+        public Escudos Escudos { get; set; }
+    }
+}
