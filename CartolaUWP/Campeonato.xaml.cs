fbc6f6641215e0b50a414c70a260a319ff7db371
Francisco Jackson da Silva Castro <jack.vasc@yahoo.com.br>
2017-09-28 23:57:04 -0300

    Add screen of Rodada

--- CartolaUWP/Campeonato.xaml.cs
+++ CartolaUWP/Campeonato.xaml.cs
@@ -10,12 +10,11 @@ namespace CartolaUWP
     {
 
         const string URL_RODADAS = "https://api.cartolafc.globo.com/rodadas";
+        const string URL_PARTIDAS = "https://api.cartolafc.globo.com/partidas";
 
         public Campeonato()
         {
             this.InitializeComponent();
-
-            this.LoadCampeonatos();
         }
 
         public void LoadCampeonatos()
@@ -43,6 +42,47 @@ namespace CartolaUWP
                 }
             },
             request);
+        }
+
+        private void LoadPartidas()
+        {
+            Uri uri = new Uri(URL_PARTIDAS, UriKind.Absolute);
+
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "GET";
+            request.Accept = "application/json";
+            request.Headers[HttpRequestHeader.UserAgent] = ".NET Framework Test Client";
+            request.BeginGetResponse((result) =>
+            {
+                var req = (HttpWebRequest)result.AsyncState;
+                var response = req.EndGetResponse(result);
+                var stream = response.GetResponseStream();
+                if (stream != null)
+                {
+                    var serializer = new DataContractJsonSerializer(typeof(List<Partida>));
+                    var Partidas = (List<Partida>)serializer.ReadObject(stream);
+
+                    this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        this.DataContext = Partidas;
+                    }).AsTask().Wait();
+                }
+            },
+            request);
+        }
+
+        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var pivot = sender as Pivot;
+            var item = pivot.SelectedItem as PivotItem;
+
+            if (item.Name == "campeonato")
+            {
+                this.LoadCampeonatos();
+            } else if (item.Name == "partidas")
+            {
+                this.LoadPartidas();
+            }
         }
     }
 }
