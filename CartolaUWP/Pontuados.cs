87b3f9a81e67027d3c8f96ee01621936eebb0a57
Roger Silva <rogersilva84@gmail.com>
2017-09-30 14:51:56 -0300

    Progress

---
+++ CartolaUWP/Pontuados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartolaUWP
+{
+    [DataContract]
+    class Pontuados
+    {
+        public Pontuados()
+        {
+        }
+
+        [DataMember(Name = "rodada")]
+        public int Rodada { get; set; }
+
+        [DataMember(Name = "total_atletas")]
+        public int TotalAtletas { get; set; }
+    }
+}
